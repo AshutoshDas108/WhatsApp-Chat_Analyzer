d347d45037915f890d64deedc3647cb43a4eded9
AshutoshDas108 <112765623+AshutoshDas108@users.noreply.github.com>
2023-05-15 01:07:47 +0530

    Update helper.py
    Features Added:
      1.Statistics of messages 
      2.Active User
      3.Word Cloud
      4.Most Frequent words

--- helper.py
+++ helper.py
@@ -1,3 +1,8 @@
+from urlextract import URLExtract
+from wordcloud import WordCloud
+
+extract = URLExtract()
+
 def fetch_stats(selected_user, df):
 
     if selected_user != 'OverAll':
@@ -8,7 +13,49 @@ def fetch_stats(selected_user, df):
     words = []
     for m in df['message']:
         words.extend(m.split())
-    return num_messages, len(words)
+
+    num_media_messages = df[df['message'] == '<Media omitted>\n'].shape[0]
+
+    links = []
+    for m in df['message']:
+        links.extend(extract.find_urls(m))
+
+    return num_messages, len(words), num_media_messages, len(links)
+
+def fetch_busy_user(df):
+    x = df['user'].value_counts().head()
+    df = round((df['user'].value_counts() / df.shape[0]) * 100, 2).reset_index().rename(
+        columns={'index': 'Name', 'user': 'Percentage_of_Message'})
+
+    return x, df
+
+def create_wordcloud(selected_user, df):
+
+    if selected_user != 'OverAll':
+        df = df[df['user'] == selected_user]
+
+    wc = WordCloud(width=500, height=500, min_font_size=10, background_color='white')
+    df_wc = wc.generate(df['message'].str.cat(sep=" "))
+    return df_wc
+    
+def most_common_words(selected_user, df):
+    if selected_user != 'OverAll':
+        df = df[df['user'] == selected_user]
+        
+    temp = df[df['user'] != 'group_notification']
+    temp = temp[temp['user'] != '<Media omitted>\n']
+    
+    words = []
+    
+    for message in temp['message']:
+        for word in message.lower().split():
+            if word not in stop_words:
+                words.append(word)
+    
+
+    
+
+
 
 
 
